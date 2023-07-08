3f1f1be94b1a83a1c72747dcb15273c8000dd3cf
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-08 18:02:34 -0300

    feat: adding mariadb datasource interface

---
+++ src/api/interface/IMariadbDataSource.ts
@@ -0,0 +1,9 @@
+import type IFileEntity from './IFileEntity'
+import type ILogEntity from './ILogEntity'
+import type IUserEntity from './IUserEntity'
+
+interface IMariadbDataSource {
+
+}
+
+export default IMariadbDataSource
