43fc89702d1408d09a1b4e68e7e9f040c4c1df50
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-11 23:46:26 -0300

    fix: removing incorrect import

--- src/data/MariadbDataSource.ts
+++ src/data/MariadbDataSource.ts
@@ -5,7 +5,7 @@ import type { Connection, Pool } from 'mariadb'
 
 import type IDatabaseHelper from '../api/interface/IDatabaseHelper'
 import type IMariadbDataSource from '../api/interface/IMariadbDataSource'
-import type IMaster from '../api/interface/IMaster'
+import type IServant from '../api/interface/IServant'
 import type IBattle from '../api/interface/IBattle'
 import type ILogEntity from '../api/interface/ILogEntity'
 
